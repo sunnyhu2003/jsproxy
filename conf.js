14ccb7af879e2e020ac1087089f6b5600fbd5e22
sunnyhu2003 <1287708285@qq.com>
2019-08-20 18:23:58 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -41,8 +41,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
-  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'cf' : 'mysite',
+  node_default: 'cf',
+  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'us' : 'mysite',
 
   /**
    * 加速节点
